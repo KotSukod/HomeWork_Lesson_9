09463b8d4e1f25858c546b22e76e005692beb03b
KotSukod <XaoS.07@mail.ru>
2022-10-18 11:01:59 +1000

    задание 1

---
+++ Task_1/Program.cs
@@ -0,0 +1,9 @@
+﻿//  Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+string Sum(int a)
+{
+    if (a >= 1) return $"{a} " + Sum(a-1);
+    else return String.Empty;
+}
+Console.WriteLine(Sum(num));
